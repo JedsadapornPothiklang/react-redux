98c72cc1ef01d7c8ded0b53d9eda68ad7f70c4d1
JedsadapornPothiklang <jedsadaporn_p@cmu.ac.th>
2026-05-05 14:33:38 +0700

    react-redux-session2

--- acadeMate/src/main.jsx
+++ acadeMate/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'; // NEW ← react-redux bridge
+import { store } from './app/store';
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+<React.StrictMode>
+<Provider store={store}> 
+<App />
+</Provider>
+</React.StrictMode>
 )
