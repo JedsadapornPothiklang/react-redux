b2e51f3609553b38f66bc33629f60d7b97854f7e
JedsadapornPothiklang <jedsadaporn_p@cmu.ac.th>
2026-05-07 20:47:47 +0700

    fix css

--- acadeMate/src/App.jsx
+++ acadeMate/src/App.jsx
@@ -11,16 +11,16 @@ function App() {
   dispatch(fetchStudents()); // load data from API when app starts
 }, [dispatch]);
 return (
-<div className="app-container">
-<header className="app-header">
-<h1>AcadeMate</h1>
-</header>
-<main className="app-main">
-<GpaSummary />
-<AddStudentForm />
-<StudentTable />
-</main>
-</div>
+    <div className="app-container">
+        <header className="app-header">
+            <h1>AcadeMate</h1>
+        </header>
+        <main className="app-main">
+            <GpaSummary />
+                <AddStudentForm />
+            <StudentTable />
+        </main>
+    </div>
 );
 }
 export default App;
